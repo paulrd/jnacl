28efb5880dac65a0d38a7263ebb85c0526018f03
Paul Dumais <paul@unstate.ca>
2015-04-23 10:05:39 -0600

    Turned into Leiningen project for Clojure

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jnacl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject jnacl "0.1.0-SNAPSHOT"
+  :description "Pure Java implementation of the NaCl curve25519xsalsa20poly1305 crypto algorithm"
+  :url "https://github.com/neilalexander/jnacl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :java-source-paths ["src"])
